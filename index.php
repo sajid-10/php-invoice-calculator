a14b4c7a10889da974f8eecab09130a1784cc0d7
AUSTo Jay <sajidalaminsaa2003@gmail.com>
2025-12-14 12:49:54 +0600

    skeleton code

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "Hello World!";
+function calculateInvoiceTotal(array $products, float $taxrate = 0, float $couponAmount = 0): float
+{
+    //implement
+    return 0;
+}
+
+$products = [
+    ['unitPrice' => 200, 'quantity' => 2],
+    ['unitPrice' => 150, 'quantity' => 3],
+
+];
+
+$taxrate = 15;
+$couponAmount = 50;
+echo "Total Invoice Amount: " . calculateInvoiceTotal($products, $taxrate, $couponAmount);
 ?>
